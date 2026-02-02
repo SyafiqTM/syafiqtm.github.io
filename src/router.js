d6be31a8f2e8e2979402fa02af55913b06a3dd7b
syafiq <syafiq.zahir94@gmail.com>
2026-02-02 22:05:24 +0800

    github pages route

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import Blog from './pages/Blog.vue'
 import BlogDetail from './pages/BlogDetail.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
